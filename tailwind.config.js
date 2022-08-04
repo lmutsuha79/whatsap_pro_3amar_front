b3ac183fca6c597544dc1e9bf23b49c8b1691d22
lamani79 <lmutsuha79@gmail.com>
2022-08-04 11:07:26 +0100

    add the hearo section of the main page

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,7 @@ module.exports = {
         "main-blue": "#094067",
         "second-blue": "#5F6C7B",
         "main-gray": "#D9D9D9",
+        "light-blue": "#3DA9FC"
       },
       fontFamily: {
         Changa: ["Changa", "sans-serif"],
